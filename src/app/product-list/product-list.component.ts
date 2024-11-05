a2351d78f381b759310a1d947b3cd1dbfd9c0ef2
Gehad Hisham <Gehad_Hisham@Tascairo.com>
2024-11-05 16:10:22 +0200

    make product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  name: string = 'samsung';
-  price: number = 150;
-  color: string = 'red';
+  addToCart: number = 0;
+  product = {
+    pImage: '/assets/samsung.webp',
+    name: 'samsung',
+    price: 150,
+    color: 'red',
+    discount: 8.5,
+    inStock: 10,
+  };
+
+  getDiscountedPrice() {
+    return (
+      this.product.price -
+      this.product.price * (this.product.discount / 100)
+    ).toFixed(2);
+  }
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) {
+      this.addToCart++;
+    }
+  }
 }

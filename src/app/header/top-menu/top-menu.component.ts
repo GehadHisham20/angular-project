6eb1b995bef7135024470bd20de55fbafeb98e2d
Gehad Hisham <Gehad_Hisham@Tascairo.com>
2025-01-14 09:55:36 +0200

    make a service

--- src/app/header/top-menu/top-menu.component.ts
+++ src/app/header/top-menu/top-menu.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { SubscribeService } from '../../Services/subscribe.service';
 
 @Component({
   selector: 'top-menu',
   templateUrl: './top-menu.component.html',
   styleUrls: ['./top-menu.component.css'],
+  providers: [SubscribeService],
 })
-export class TopMenuComponent {}
+export class TopMenuComponent {
+  constructor(private subscribeService: SubscribeService) {}
+  OnSubscribeClicked() {
+    this.subscribeService.OnSubscribeClicked();
+  }
+}
